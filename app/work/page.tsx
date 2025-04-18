e8a1173390b159e1723076ad44811be87085d78b
Sarun Daunghirun <sdsarun@outlook.com>
2025-04-19 04:34:19 +0700

    feat(work): init work page

---
+++ app/work/page.tsx
@@ -0,0 +1,15 @@
+import { Main } from '@/core/components/layout/main'
+import { Metadata } from 'next'
+import React from 'react'
+
+export const metadata: Metadata = {
+  title: "work"
+}
+
+export default function WorkPage() {
+  return (
+    <Main>
+      WorkPage
+    </Main>
+  )
+}
