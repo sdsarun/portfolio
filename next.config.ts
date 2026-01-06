0181371abcf70794b27958bacea7fc1d16d7ba9d
Sarun Daunghirun <sdsarun@outlook.com>
2026-01-06 23:19:01 +0700

    chore(next-config): add debug cache on local

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  logging: {
+    fetches: {
+      fullUrl: true
+    }
+  },
   images: {
     remotePatterns: [
       {
